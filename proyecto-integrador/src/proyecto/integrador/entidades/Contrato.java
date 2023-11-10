2b8ddb8a7b5c821236547bf1bdfe76f4fd2375d3
Andres Luna <65298144+Luna201@users.noreply.github.com>
2023-11-10 20:40:30 -0300

    Update Contrato.java

--- proyecto-integrador/src/proyecto/integrador/entidades/Contrato.java
+++ proyecto-integrador/src/proyecto/integrador/entidades/Contrato.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectointegradorpractica.entidades;
+package proyecto.integrador.entidades;
 
 import java.util.Scanner;
 
